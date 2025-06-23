22388963c6e0ed155f8fb3ae48d5e4e4d8e1f8af
Tushar <103923200+tushaarrr@users.noreply.github.com>
2025-06-24 01:40:49 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,26 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import Script from 'next/script';
+
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* ✅ Google Analytics GA4 */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-LXCW5CCPJ4"
-        strategy="afterInteractive"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-LXCW5CCPJ4');
-          `,
-        }}
-      />
-      
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LXCW5CCPJ4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LXCW5CCPJ4');
+            `,
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
     </>
-  );
+  )
 }
